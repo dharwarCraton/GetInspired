30b2dca1e31307be85de87e0108a01c59819946b
dharwarCraton <fnalam@bsugmail.net>
2017-05-25 14:35:47 -0400

    Initial Commit

---
+++ GetInspired/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetInspired
+//
+//  Created by Farah Alam on 5/25/17.
+//  Copyright © 2017 Farah Alam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

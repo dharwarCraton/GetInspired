d5e5751206b9f8e2f7d1219dc2769b03fe7dc6ea
dharwarCraton <fnalam@bsugmail.net>
2017-05-25 15:54:22 -0400

    Actions and Outlets connect from second VC’s UI to second VC in Swift.

---
+++ GetInspired/QuoteCatcherViewController.swift
@@ -0,0 +1,38 @@
+//
+//  QuoteCatcherViewController.swift
+//  GetInspired
+//
+//  Created by Farah Alam on 5/25/17.
+//  Copyright © 2017 Farah Alam. All rights reserved.
+//
+
+import UIKit
+
+class QuoteCatcherViewController: UIViewController {
+    /*
+        OUTLETS connected here
+    */
+    
+    @IBOutlet weak var quoteImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    /*
+         ACTIONS connected here
+    */
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+    
+}
